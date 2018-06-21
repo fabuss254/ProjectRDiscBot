f9339c1ed87d301db51e01b2c02dc189f478412e
fabuss254 <fabuss254@outlook.fr>
2018-06-22 01:54:43 +0200

    Create Test.js

---
+++ .gitignore/Test.js
@@ -0,0 +1,4 @@
+const Discord = require("discord.js");
+var bot = new Discord.Client();
+bot.login("NDU4MzE1NTQ2OTA4MTY0MTE5.Dg3L_A.wpK8utK_P-6dj4zziIBcQ5Ky824");
+console.log("Login succesfully!");
